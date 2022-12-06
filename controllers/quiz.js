2a4c2f9398afe800414a00250ffedfa065cc3468
Nazar Pryymak <nazarlviv07@gmail.com>
2022-12-07 01:00:48 +0200

    Move number of questions to page questions

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -51,12 +51,36 @@ exports.index = function(req, res) {
     }
     _page = 'quiz-index-user';
   }
+  models.Quiz_question.findAll().then(function(quiz_questions) {
+    simple_questions = 0;
+    normal_questions = 0;
+    complicated_questions = 0;
+    for (const key in quiz_questions) {
+      complexity = quiz_questions[key].complexity
+      if (complexity == "simple"){
+        simple_questions += 1
+      }else if (complexity == "normal"){
+        normal_questions += 1
+
+      }else if (complexity == "complicated"){
+        complicated_questions += 1
+      }
+      console.log("simple_questions = " + simple_questions)
+      console.log("normal_questions = " + normal_questions)
+      console.log("complicated_questions = " + complicated_questions)
+    }
+    total_questions = simple_questions + normal_questions + complicated_questions;
+    });
 
   models.Quiz_question.findAll(options).then(
     function(quizzes) {
       res.render('quizzes/index.ejs', {
         page: _page,
         quizzes: quizzes,
+        total_questions: total_questions,
+        simple_questions: simple_questions,
+        normal_questions: normal_questions,
+        complicated_questions: complicated_questions,
         errors: []
       });
     }
